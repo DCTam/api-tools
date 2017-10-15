d19f5dad8739ce61674a870d11c99a84bda5aac5
Daniel Tam <ctam.daniel@gmail.com>
2017-10-15 16:05:12 -0700

    Added textarea to submit json

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 'use strict';
 const app = require('express')();
 const path = require('path');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname + '/src/views/testing.html'));
+});
+
+app.get('/mock', (req, res) => {
+	res.sendFile(path.join(__dirname + '/src/views/mock.html'));
+});
+
+app.post('/mock', (req, res) => {
+	res.send(req.body.jsonInput);
 });
 
 app.listen(3000, () => {
